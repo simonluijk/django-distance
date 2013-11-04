39e868aa936cb67996ae295093bde67e00907115
Simon Luijk <simon@simonluijk.com>
2013-11-04 16:32:27 +0100

    Dynamically set field string
    Signed-off-by: Simon Luijk <simon@simonluijk.com>

--- distance/migrations/0001_initial.py
+++ distance/migrations/0001_initial.py
@@ -4,6 +4,13 @@ from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
+try:
+    from localflavor.us.models import USStateField
+    us_state_field = 'localflavor.us.models.USStateField'
+except ImportError:
+    from django.contrib.localflavor.us.models import USStateField
+    us_state_field = 'django.contrib.localflavor.us.models.USStateField'
+
 
 class Migration(SchemaMigration):
 
@@ -15,7 +22,7 @@ class Migration(SchemaMigration):
             ('longitude', self.gf('django.db.models.fields.FloatField')()),
             ('code', self.gf('django.db.models.fields.CharField')(max_length=5)),
             ('city', self.gf('django.db.models.fields.CharField')(max_length=128)),
-            ('state', self.gf('django.contrib.localflavor.us.models.USStateField')(max_length=2)),
+            ('state', self.gf(us_state_field)(max_length=2)),
         ))
         db.send_create_signal(u'distance', ['Zip'])
 
@@ -33,7 +40,7 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'latitude': ('django.db.models.fields.FloatField', [], {}),
             'longitude': ('django.db.models.fields.FloatField', [], {}),
-            'state': ('django.contrib.localflavor.us.models.USStateField', [], {'max_length': '2'})
+            'state': (us_state_field, [], {'max_length': '2'})
         }
     }
 
